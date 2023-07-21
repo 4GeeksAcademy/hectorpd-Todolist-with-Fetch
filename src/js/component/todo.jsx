fb5e70b14be2d49850ac6ac2dae6f9c71fe00d52
Hector Padron <helpad93@gmail.com>
2023-07-21 21:57:25 +0000

    Add: index.css / Change: todo.jsx

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export const ToDo = () => {
     const [List, setList] = useState ([]);
+    const [newTaskTitle, setNewTaskTitle] = useState("");
     const host= "https://jsonplaceholder.typicode.com";
 
     const fetchList = async  () => {
@@ -20,7 +21,24 @@ export const ToDo = () => {
                 console.log(response.status, response.statusText)
             }
     }; 
-        
+    const handleAddTask = (task) => {
+        if (newTaskTitle.trim() !== "") {
+            const newTask = {
+                id: list.length + 1,
+                title: "newTaskTitle",
+                completed: false,
+            };
+        }
+        // Clonamos el array actual y agregamos la nueva tarea
+        setTasks([...tasks, task]);
+        setNewTaskTitle("");
+    };
+    
+    const handleDeleteTask = (taskId) => {
+        // Filtramos las tareas para eliminar la que coincida con el id proporcionado
+        const updatedTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(updatedTasks);
+    };
             
     
     useEffect(() => {
@@ -31,11 +49,21 @@ export const ToDo = () => {
         <div>
             <h3>List with API </h3>
             <p>Fuente: {host}</p>
-            <ul>
-                { !List ? "On load" :
-                    List.map((todo, index) => (
-                        <li key={index}>
-                            {todo.title} - {todo.completed ? (
+            <div>
+                <input
+                    type="text"
+                    value={newTaskTitle}
+                    onChange={(e) => setNewTaskTitle(e.target.value)}
+                    placeholder="Escribir nueva tarea"
+                />
+                <button onClick={handleAddTask}>Agregar tarea</button>
+            </div>
+            <div className="listcontainer">
+                <ul className="list">
+                    { !List ? "On load" :
+                        List.map((todo, index) => (
+                            <li key={index} className="tasklist">
+                                {todo.title} - {todo.completed ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-journal-check" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
                                 <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z" />
@@ -46,10 +74,15 @@ export const ToDo = () => {
                                 <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
                                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                                 </svg>
-                            )}
+                                )}
+                                <button onClick={() => handleDeleteTask(task.id)}>
+                                    Eliminar
+                                </button>
                             </li>
-                    ))}
-            </ul>
+                        ))}
+                </ul>
+            </div>
+            
         </div>
     );
 }
